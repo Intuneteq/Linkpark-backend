8e3f4a9db62a0971e58af99bdb2047e8eb8f0313
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-09-18 07:33:04 +0100

    cors production

--- Config/corsOptions.js
+++ Config/corsOptions.js
@@ -2,7 +2,7 @@ const whiteList = require('./whiteList');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (whiteList.indexOf(origin) !== -1 || !origin) { //remove !origin for production
+        if (whiteList.indexOf(origin) !== -1) { //remove !origin for production
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
